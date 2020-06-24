9882d7ad52a971d922694900ce4032acc5a7fdc4
Claudio Olivares <claudio.olivares@zentagroup.com>
2020-06-24 19:46:52 -0400

    bootstrap install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MyNavbarComponent } from './my-navbar/my-navbar.component';
+import { MyNavbarComponent } from './components/my-navbar/my-navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MainComponent } from './components/main/main.component';
+
+// Angular Material
+
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNavbarComponent
+    MyNavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +29,11 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+
+    // angular material
+
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
